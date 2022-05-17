e506040ea5d190530b200d778357eb5fb6df6542
Jordan Wilson <biggyj_dev@outlook.com>
2022-05-18 00:12:39 +0100

    CSS Complete

--- digital-business-card/src/Components/Footer.js
+++ digital-business-card/src/Components/Footer.js
@@ -3,19 +3,13 @@ import { BsInstagram, BsGithub } from "react-icons/bs";
 
 function Footer() {
   return (
-    <div>
-      <ul>
-        <li>
-          <a href='https://www.instagram.com/biggyj_dev/?hl=en-gb'>
-            <BsInstagram />
-          </a>
-        </li>
-        <li>
-          <a href='https://github.com/BiggyJDev'>
-            <BsGithub />
-          </a>
-        </li>
-      </ul>
+    <div className='footer'>
+      <a href='https://www.instagram.com/biggyj_dev/?hl=en-gb'>
+        <BsInstagram />
+      </a>
+      <a href='https://github.com/BiggyJDev'>
+        <BsGithub />
+      </a>
     </div>
   );
 }
