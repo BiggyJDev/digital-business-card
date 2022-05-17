02de969ff49c0cab27fb3767013d143652d0437f
Jordan Wilson <biggyj_dev@outlook.com>
2022-05-17 20:52:00 +0100

    Convert HTML Markup into Components

---
+++ static-info-page/src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <img src='/logo192.png' alt='React Logo' width={"80px"} />
+          <h1>React</h1>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
