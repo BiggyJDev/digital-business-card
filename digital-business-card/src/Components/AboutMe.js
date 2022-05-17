0ceca612f60ee0d99edc4e0ed4f212ad799d2311
Jordan Wilson <biggyj_dev@outlook.com>
2022-05-18 00:01:14 +0100

    AboutMe CSS Complete

--- digital-business-card/src/Components/AboutMe.js
+++ digital-business-card/src/Components/AboutMe.js
@@ -13,35 +13,38 @@ function AboutMe() {
       </div>
       <div className='contact-links'>
         <button>
-          <a href='mailto: biggyj_dev@outlook.com'>
-            <HiOutlineMail /> Email
+          <a className='email' href='mailto: biggyj_dev@outlook.com'>
+            <HiOutlineMail /> <span>Email</span>
           </a>
         </button>
         <button>
           <a href='https://www.linkedin.com/in/jordan-wilson-310030151/'>
-            <FaLinkedin /> LinkedIn
+            <FaLinkedin />
+            <span> LinkedIn</span>
           </a>
         </button>
       </div>
-      <h1>About</h1>
-      <p>
-        I am a junior front end developer who LOVES working with React. I am
-        originally from a construction background but decided to make the change
-        during the Covid-19 Pandemic. I am currently studying a BSc in Cyber
-        Security in my spare time at the Open University. I am obsessed with
-        learning and constantly wanting to progress.
-      </p>
+      <div className='about'>
+        <h1>About</h1>
+        <p>
+          I am a junior front end developer who LOVES working with React. I am
+          originally from a construction background but decided to make the
+          change during the Covid-19 Pandemic. I am currently studying a BSc in
+          Cyber Security in my spare time at the Open University. I am obsessed
+          with learning and constantly wanting to progress.
+        </p>
 
-      <h1>Interests</h1>
-      <p>
-        In my spare time other than studying I like to unwind by :
-        <ul>
-          <li>Spending time with my girlfriend</li>
-          <li>Gaming (LoL and WoW)</li>
-          <li>Going to the gym</li>
-          <li>and playing rugby</li>
-        </ul>
-      </p>
+        <h1>Interests</h1>
+        <p>
+          In my spare time other than studying I like to unwind by:
+          <ul>
+            <li>Spending time with my girlfriend</li>
+            <li>Gaming (LoL and WoW)</li>
+            <li>Going to the gym</li>
+            <li>Playing rugby</li>
+          </ul>
+        </p>
+      </div>
     </>
   );
 }
