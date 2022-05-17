bfb0cffec3d6ae8c879a9021ed9d8e00988a50a0
Jordan Wilson <biggyj_dev@outlook.com>
2022-05-17 19:58:08 +0100

    HTML Markup

--- static-info-page/src/App.js
+++ static-info-page/src/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <img src='/logo192.png' alt='React Logo' />
+      <h1> Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was orginally created by Jordan Walke</li>
+        <li>Has over 100k stars on GitHub</li>
+        <li>Is maintained by facebook</li>
+        <li>
+          It powers thousands of enterprise apps, including mobile apps using
+          its native version
+        </li>
+      </ul>
     </div>
   );
 }
