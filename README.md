a1715343bdaa24c74d33873304316315b20ffe5b
Jordan Wilson <biggyj_dev@outlook.com>
2022-05-17 21:18:47 +0100

    Nav Bar Styling

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ So I will start from the very basics and move my way up.
 Here is one of my first projects which is just a static react app that contains some facts about react.
 
 I hope you enjoy :)
+
+So how did I make this Static Page
+
+1. Create React App and do a bit of spring cleaning
+2. Take the content from our Figma Design and markup the html
+3. Take our HTML Markup and create suitable Components
--- static-info-page/src/Components/Header.js
+++ static-info-page/src/Components/Header.js
@@ -4,9 +4,11 @@ function Header() {
   return (
     <>
       <header>
-        <nav>
-          <img src='/logo192.png' alt='React Logo' width={"80px"} />
-          <h1>React</h1>
+        <nav className='nav'>
+          <img className='nav-image' src='/logo192.png' alt='React Logo' />
+          <ul className='nav-items'>
+            <li>About</li> <li>Pricing</li> <li>Contact</li>
+          </ul>
         </nav>
       </header>
     </>
